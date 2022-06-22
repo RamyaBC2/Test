70404d9d44c1052045f03ccbf685fe58d7bb7423
admin <admin@DESKTOP-LLREKF9>
2022-06-22 20:13:01 +0530

    First Release

---
+++ Frame/src/Jeneric_script/Constant_Data.java
@@ -0,0 +1,11 @@
+package Jeneric_script;
+
+public interface Constant_Data {
+String gecko_key="webdriver.gecko.driver";
+String gecko_value="./Softwares/geckodriver.exe";
+
+String chrome_key="webdriver.chrome.driver";
+String chrome_value="./Softwares/chromedriver.exe";
+
+String url="https://www.facebook.com/";
+}
